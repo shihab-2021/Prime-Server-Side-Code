58fbc5791b938e0bc3a27aedeb870316d10e78d2
Shajibul Alam Shihab <82690571+shihab-2021@users.noreply.github.com>
2022-11-13 18:06:21 +0600

    user updating code

--- index.js
+++ index.js
@@ -50,18 +50,18 @@ async function run() {
       }
     });
     // put user for google login
-    // app.put("/users", async (req, res) => {
-    //   const user = req.body;
-    //   const filter = { email: user.email };
-    //   const options = { upsert: true };
-    //   const updateDoc = { $set: user };
-    //   const result = await usersCollection.updateOne(
-    //     filter,
-    //     updateDoc,
-    //     options
-    //   );
-    //   res.json(result);
-    // });
+    app.put("/users", async (req, res) => {
+      const user = req.body;
+      const filter = { email: user.email };
+      const options = { upsert: true };
+      const updateDoc = { $set: user };
+      const result = await usersCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      res.json(result);
+    });
 
     // user profile update api here
     app.put("/profile-update", async (req, res) => {
